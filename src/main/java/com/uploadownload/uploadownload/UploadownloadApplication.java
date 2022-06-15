94a352d6686df7bb24c7f3119a00fc7d433fb830
Daniel Lorenzo <lorenzccaz@gmail.com>
2022-06-14 19:04:02 -0500

    Archivos en la base de datos

---
+++ src/main/java/com/uploadownload/uploadownload/UploadownloadApplication.java
@@ -0,0 +1,13 @@
+package com.uploadownload.uploadownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uploadownload/uploadownload/UploadownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadownload.uploadownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
